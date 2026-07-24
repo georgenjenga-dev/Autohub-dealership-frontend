90cabce64068837e5165326bfb1407d3e2e94ee6
kihoto <george.njenga@student.moringaschool.com>
2026-07-24 23:15:14 +0300

    added about

--- src/App.jsx
+++ src/App.jsx
@@ -21,66 +21,69 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Navbar />
 
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/cars/:id" element={<CarDetails />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <main className="page-content">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/cars/:id" element={<CarDetails />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected Routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/my-reservations"
-          element={
-            <ProtectedRoute>
-              <MyReservations />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/my-reservations"
+            element={
+              <ProtectedRoute>
+                <MyReservations />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/my-payments"
-          element={
-            <ProtectedRoute>
-              <MyPayments />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/my-payments"
+            element={
+              <ProtectedRoute>
+                <MyPayments />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/my-inquiries"
-          element={
-            <ProtectedRoute>
-              <MyInquiries />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-  path="/admin-dashboard"
-  element={
-    <AdminRoute>
-      <AdminDashboard />
-    </AdminRoute>
-  }
-/>
-      </Routes>
+          <Route
+            path="/my-inquiries"
+            element={
+              <ProtectedRoute>
+                <MyInquiries />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            }
+          />
+        </Routes>
+      </main>
 
       <Footer />
-    </>
+    </div>
   );
 }
 
